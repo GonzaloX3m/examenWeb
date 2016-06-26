6f73205e1d7bc4f92b44f525714cc67103ee7f64
Gonzalo Zúñiga <astaroth_x3m@hotmail.com>
2016-06-25 23:27:37 -0400

    Script sql

---
+++ script.sql
@@ -0,0 +1,16 @@
+create database sistema_Ventas;
+use sistema_Ventas;
+
+create table producto{
+	codigo_producto int auto_increment,
+	nombre_producto varchar(30),
+	codigo_tipo int,
+	precio_producto int
+	primary key(codigo_producto)
+};
+
+insert into producto values();
+
+create table tipo{
+
+	}
